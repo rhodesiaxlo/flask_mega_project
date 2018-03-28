5399505dc95c6ce696c259ade734d19eb55545a5
rhode <rhodesiax.lo@gmail.com>
2018-03-28 23:29:55 +0800

    first view

--- app_package/route.py
+++ app_package/route.py
@@ -1,5 +1,18 @@
 from app_package import app
 
+## 无法热修改
+# 可以有格式
+
 @app.route("/")
+@app.route("/index")
 def hello():
-    return "hello world !"
+    user = {"name": "lushixin"}
+    return '''
+    <html>
+        <head>
+            <title>home page - my blog</title>
+        </head>
+        <body>
+            <p> hello <strong>''' + user['name'] + '''</strong>
+        </body>
+    </html>'''
